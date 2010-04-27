907b5a1b93a75215c7c1e6e10a114fe7b5a06a91
Eric Bannatyne <eric.bannatyne@gmail.com>
2010-04-26 21:24:19 -0700

    Setting default to command-line interface, instead of crappy GUI.

--- src/notefile/Notefile.java
+++ src/notefile/Notefile.java
@@ -3,14 +3,14 @@
 
 package notefile;
 
-//import notefile.CommandLine;
-import notefile.gui.*;
+import notefile.CommandLine;
+//import notefile.gui.*;
 
 public class Notefile {
 
 	public static void main(String[] args) throws Exception {
-		// new CommandLine(true);
-		new GUI();
+		new CommandLine(true);
+		//new GUI();
 	}
 
 }
